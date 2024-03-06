f0d2f67da76c03e10107346203dc10e871e9b70c
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-03-06 17:55:05 +0200

    feat: dialog functionality

--- javascript-2024/library/scripts/app.js
+++ javascript-2024/library/scripts/app.js
@@ -2,29 +2,45 @@
 
 const myLibrary = [];
 const addBookBtn = document.querySelector("#add-book-btn");
+// Dialog Variables
 const dialog = document.querySelector("dialog");
+const dialogForm = document.querySelector("dialog form");
+const dialogCancelBtn = document.querySelector("#dialog-cancel");
+const bookTitle = document.querySelector("#book-title-input");
+const bookAuthor = document.querySelector("#book-author-input");
+const bookPages = document.querySelector("#book-pages-input");
+const bookReadStatus = document.querySelector("#read-status-input");
 
-function Book(name, author, pages, isRead) {
-	this.name = name;
+function Book(title, author, pages, isRead) {
+	this.id = myLibrary.length;
+	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-	let bookName = "cool book";
-	let bookAuthor = "brilliant author";
-	let bookPages = "100";
-	let isRead = false;
-	myLibrary.push(new Book(bookName, bookAuthor, bookPages, isRead));
+function addBookToLibrary(title, author, pages, isRead = false) {
+	myLibrary.push(new Book(title, author, pages, isRead));
+}
+function resetDialogForm() {
+	bookTitle.value = "";
+	bookAuthor.value = "";
+	bookPages.value = "";
+	bookReadStatus.checked = false;
 }
 
-addBookToLibrary();
-console.log(myLibrary);
-
-//  function that loops through the array and displays each book on the page
-// Add a “NEW BOOK” button that brings up a form as a modal
-// Add a button on each book’s display to remove the book from the library
-// Add a button on each book’s display to change its read status
-
 addBookBtn.addEventListener("click", () => dialog.showModal());
+
+dialogForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	addBookToLibrary(
+		bookTitle.value,
+		bookAuthor.value,
+		bookPages.value,
+		bookReadStatus.checked
+	);
+	resetDialogForm();
+	dialog.close();
+});
+dialogCancelBtn.addEventListener("click", () => dialog.close());
