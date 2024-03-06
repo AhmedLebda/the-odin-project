cc1c81e6feacabcb679e7dd0b87a1ecd39e7c9e6
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-03-06 18:45:37 +0200

    feat: create book UI function

--- javascript-2024/library/scripts/app.js
+++ javascript-2024/library/scripts/app.js
@@ -11,6 +11,8 @@ const bookAuthor = document.querySelector("#book-author-input");
 const bookPages = document.querySelector("#book-pages-input");
 const bookReadStatus = document.querySelector("#read-status-input");
 
+const booksWrapper = document.querySelector(".books__wrapper");
+
 function Book(title, author, pages, isRead) {
 	this.id = myLibrary.length;
 	this.title = title;
@@ -29,6 +31,69 @@ function resetDialogForm() {
 	bookReadStatus.checked = false;
 }
 
+// function createBookElement(title, author, pages, isRead) {
+// 	const book = document.createElement("div");
+// 	const bookTitle = document.createElement("h3");
+// 	const bookAuthor = document.createElement("p");
+// 	const bookPages = document.createElement("p");
+// 	const bookControls = document.createElement("div");
+// 	const readOptionWrapper = document.createElement("div");
+// 	const readOptionLabel = document.createElement("label");
+// 	const readInputWrapper = document.createElement("div");
+// 	const readOptionInput = document.createElement("input");
+// 	const deleteBookBtn = document.createElement("button");
+// 	bookTitle.textContent = title;
+// 	bookAuthor.textContent = author;
+// 	bookPages.textContent = `${pages} pages`;
+// 	readOptionLabel.textContent = isRead ? "Read" : "Not Read";
+// 	readOptionInput.checked = isRead;
+// 	deleteBookBtn.textContent = "Delete";
+// 	book.classList.add("book");
+// 	bookTitle.classList.add("book__title");
+// 	bookAuthor.classList.add("book__author");
+// 	bookPages.classList.add("book__pages");
+// 	bookControls.classList.add("book__controls");
+// 	readOptionWrapper.classList.add("read__option__wrapper");
+// 	readInputWrapper.classList.add("read__input__wrapper");
+// 	readOptionInput.classList.add("read__status");
+// 	deleteBookBtn.classList.add("delete__book");
+// 	readOptionInput.setAttribute("type", "checkbox");
+// 	readOptionInput.setAttribute("name", "read-status");
+// 	book.appendChild(bookTitle);
+// 	book.appendChild(bookAuthor);
+// 	book.appendChild(bookPages);
+// 	book.appendChild(bookControls);
+// 	bookControls.appendChild(readOptionWrapper);
+// 	readOptionWrapper.appendChild(readOptionLabel);
+// 	readOptionWrapper.appendChild(readInputWrapper);
+// 	readInputWrapper.appendChild(readOptionInput);
+// 	bookControls.appendChild(deleteBookBtn);
+// 	booksWrapper.appendChild(book);
+// }
+
+function createBookElement(title, author, pages, isRead) {
+	const book = document.createElement("div");
+	book.classList.add("book");
+	book.innerHTML = `<h3 class="book__title">${title}</h3>
+	<p class="book__author">${author}</p>
+	<p class="book__pages">${pages} pages</p>
+	<div class="book__controls">
+		<div class="read__option__wrapper">
+			<label for="read-status">${isRead ? "read" : "not read"}</label>
+			<div class="read__input__wrapper">
+				<input
+					type="checkbox"
+					class="read__status"
+					name="read-status"
+					id="read-status"
+				/>
+			</div>
+		</div>
+		<button class="delete__book">delete</button>
+	</div>`;
+	booksWrapper.appendChild(book);
+}
+
 addBookBtn.addEventListener("click", () => dialog.showModal());
 
 dialogForm.addEventListener("submit", (e) => {
@@ -42,5 +107,13 @@ dialogForm.addEventListener("submit", (e) => {
 	);
 	resetDialogForm();
 	dialog.close();
+
+	const createdBook = myLibrary[myLibrary.length - 1];
+	createBookElement(
+		createdBook.title,
+		createdBook.author,
+		createdBook.pages,
+		createdBook.isRead
+	);
 });
 dialogCancelBtn.addEventListener("click", () => dialog.close());
