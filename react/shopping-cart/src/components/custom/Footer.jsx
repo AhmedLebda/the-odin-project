fdb576f8e4d3f726d6d6bf5e819a5c3ec52401c3
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-04-26 19:36:29 +0300

    Add: footer

---
+++ react/shopping-cart/src/components/custom/Footer.jsx
@@ -0,0 +1,15 @@
+import PaymentImages from "../../Images/pay-methods.png";
+
+const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+        <footer className="mt-auto p-8 bg-gray-200 text-gray-700 text-sm flex flex-col gap-4 sm:flex-row justify-between items-center">
+            <p className="pb-2">
+                Osiris Outlet &copy; {year}, all rights reserved{" "}
+            </p>
+            <img src={PaymentImages} alt="Payment Methods" />
+        </footer>
+    );
+};
+export default Footer;
--- react/shopping-cart/src/pages/Layout.jsx
+++ react/shopping-cart/src/pages/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/custom/Navbar";
+import Footer from "../components/custom/Footer";
 const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
             <Outlet></Outlet>
-            <h2>Footer Layout...</h2>
+            <Footer />
         </div>
     );
 };
