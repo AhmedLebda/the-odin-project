7af57e45319b95afb03a616147734b0af182ba5d
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-05-29 21:24:20 +0300

    Add: post model

---
+++ node/members-only/models/post_model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: ["true", "messages must have a title"],
+            minlength: [3, "Title is too short"],
+        },
+        content: {
+            type: String,
+            required: ["true", "posts must have content"],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Post", postSchema);
