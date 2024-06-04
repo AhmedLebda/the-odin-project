40a5c0dd8a11eea8414e5d86d0c384d02192d3fe
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-06-04 16:45:29 +0300

    Add: material ui signup and signin pages

--- node/blog-api/frontend/src/routes.jsx
+++ node/blog-api/frontend/src/routes.jsx
@@ -2,7 +2,8 @@ import MainLayout from "./pages/layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/404";
-
+import SignUp from "./pages/user/SignUp";
+import SignIn from "./pages/user/SignIn";
 const routes = [
     {
         path: "/",
@@ -16,6 +17,14 @@ const routes = [
             {
                 path: "about",
                 element: <About />,
+            },
+            {
+                path: "sign-up",
+                element: <SignUp />,
+            },
+            {
+                path: "sign-in",
+                element: <SignIn />,
             },
         ],
     },
